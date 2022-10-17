8dbb35cf99a6b23b62862a66735124120b3a0f10
garayot <garayot@gmail.com>
2022-10-18 07:59:36 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Information gathered</title>
+</head>
+<body>
+
+    <?php 
+        &usersName= &_POST['userName'];
+        &streetAddress= &_POST['streetAddress'];
+        &cityAddress= &_POST['cityAddress'];
+
+        echo &usersName . "</br>";
+    ?>
+    
+</body>
+</html>
